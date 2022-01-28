63687c89b58d862328da69447b2b1be85f240cd7
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-01-28 14:28:07 +0700

    update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ nocache('./denz.js', module => console.log(color('|TRM|'), color(`${module} Upda
 
 async function starts() {
 denz.autoReconnect = ReconnectMode.onConnectionLost
-    denz.version = [2, 2140, 12]
+    denz.version = [2, 2143, 3]
     denz.logger.level = 'warn'
     denz.browserDescription = ['Alyaa X Radhin','Desktop','3.0']
     await sleep(10000)
